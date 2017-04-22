b818e6af067272157999a8fef2de36bc5d4872da
Christian Cataldo <christian.cataldo@itcollege.ee>
2017-04-22 13:20:44 +0300

    Improve hw03_v01, now adds new data on existing file

--- Homework03/FirstMaven/src/ee/itcollege/maven/HW03_v01.java
+++ Homework03/FirstMaven/src/ee/itcollege/maven/HW03_v01.java
@@ -2,9 +2,12 @@ package ee.itcollege.maven;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Date;
+
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -14,6 +17,7 @@ import org.jsoup.select.Elements;
 public class HW03_v01 {
 
 	static Document document;
+	private static Path path;
 
 	static {
 		try {
@@ -29,21 +33,30 @@ public class HW03_v01 {
 
 		int nt = 0;
 		ArrayList<String> text = new ArrayList<>();
-		String info = " 📈  Estonian news in English from news.err.ee:\n"
+		String info = "\n----------------------------------------------\n "
+				+ " 📈  Estonian news in English from news.err.ee:\n"
+				+ "     (" + new Date() + ")\n"
 				+ "-----------------------------------------------\n\n";
 		text.add(info);
 
 		for (Element title : titles) {
 			nt++;
-			String line1 = (nt + ") " + title.text() + " (" + new Date() + ") " + "\n");  //parse titles
+			String line1 = (nt + ") " + title.text() + "\n");  //parse titles
 			String line2 = (" >>> " + title.attr("href") + "\n\n");  //parse title links			
 			text.add(line1 + line2);
 			String textS = text.toString().replace(",", "").replace("[", " ").replace("]", "");
 			System.out.println(textS);
-		} 
+			//print only first 10 headlines:
+			if (nt == 10) {		
+				break;
+			}
+		}
 		
+		//write to file:
 		String textS = text.toString().replace(",", "").replace("[", " ").replace("]", "");
-		Files.write(Paths.get("output/output.txt"), textS.getBytes());
+		path = Paths.get("output/output.txt");
+		Files.write(path, textS.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+		//CREATE = create new file if not exist; APPEND = add new data to same file when program is run
 
 	}
 }
