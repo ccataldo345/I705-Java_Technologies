ff5ac20ec0ac5031f1f22cde24c73bd34f7e2c1a
Christian Cataldo <christian.cataldo@itcollege.ee>
2017-05-10 00:54:31 +0300

    Add Homework 5, WIP

---
+++ Homework05/README.md
@@ -0,0 +1,32 @@
+DESCRIPTION
+------------
+
+
+Homework 5 - A fancy print method
+
+Write a method "print" (just one method!) that would work for the following lines:
+
+<pre>
+print("Names that start with A",
+  e -> e.startsWith("A"), "Mati", "Kati", "Aadu");
+
+print("Values that are greater than ten",
+  e -> e > 10, 40, 0, 4, -5, -14, 35, 1, 5, 6, 7, 8);
+</pre>
+
+The output of these lines should be:
+<pre>
+Names that start with A: Aadu
+Values that are greater than ten: 40, 35
+</pre>
+
+
+USAGE
+------------
+
+run: <br/>
+<!-- - ee.itcollege.maven.HW03_v01.java
+
+my files: <br/>
+- output.txt
+
---
+++ Homework05/fancyPrint.java
@@ -0,0 +1,37 @@
+package homework05;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class fancyPrint {
+
+	public static void main(String args[]) {
+		List<String> names = Arrays.asList("Mati", "Kati", "Aadu", "Alan");	
+		List<String> aNames = new ArrayList<>();
+
+		System.out.print("Names that start with A: ");
+		//Stream.of(names) .filter(names -> names.startsWith("A")) .forEach(System.out::println);
+		names.forEach(name -> {
+			if(name.startsWith("A")) {
+				aNames.add(name);
+				System.out.print(name + ", ");
+			}
+		});
+
+	}
+	
+//	public static int print (int a) {
+//		List<Integer> numbers = Arrays.asList(10, 40, 0, 4, -5, -14, 35, 1, 5, 6, 7, 8);	
+//		List<String> numBigTen = new ArrayList<>();
+//
+//		numbers.forEach(number -> {
+//			if(valueOf(number)>(10)) {
+//				numBigTen.add(number);
+//				System.out.println(number);
+//			}
+//		});
+//	}
+}
+
