a029db7fd7bfbd109b1dac86763d2940ab234a17
Christian Cataldo <christian.cataldo@itcollege.ee>
2017-05-16 22:45:41 +0300

    Fix in HW 5 java class name with capital letter

--- Homework05/FancyPrint.java
+++ Homework05/FancyPrint.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class fancyPrint {
+public class FancyPrint {
 
 	public static void main(String args[]) {
 		List<String> names = Arrays.asList("Mati", "Kati", "Aadu", "Alan");	
