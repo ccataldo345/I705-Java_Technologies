3c6e4a5cc442277e8113eb7b9711e377d045781a
Christian Cataldo <christian.cataldo@itcollege.ee>
2017-05-26 12:48:17 +0300

    Fix HW 5 with Predicate

--- Homework05/FancyPrint.java
+++ Homework05/FancyPrint.java
@@ -3,34 +3,27 @@ package homework05;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class FancyPrint {
 
 	public static void main(String args[]) {
-		List<String> names = Arrays.asList("Mati", "Kati", "Aadu");
-		// List<String> aNames = new ArrayList<>();
 
-		System.out.print("Names that start with A: ");
-		// Stream.of(names) .filter(names -> names.startsWith("A"))
-		// .forEach(System.out::println);
-		names.forEach(name -> {
-			if (name.startsWith("A")) {
-				// aNames.add(name);
-				System.out.print(name + " ");
+		print("Names that start with A", e -> e.startsWith("A"), "Mati", "Kati", "Aadu");
+
+		print("Values that are greater than ten", e -> e > 10, 40, 0, 4, -5, -14, 35, 1, 5, 6, 7, 8);
+	}
+
+	public static <T> void print(String A, Predicate<T> B, T... param) {
+
+		System.out.print(A + ": ");
+
+		for (T t : param) {
+			if (B.test(t)) {
+				System.out.print(t + " ");
 			}
-		});
-
-		// public static int print (int a) {
-		List<Integer> numbers = Arrays.asList(10, 40, 0, 4, -5, -14, 35, 1, 5, 6, 7, 8);
-		// List<String> numBigTen = new ArrayList<>();
+		}
+		// System.out.print(".");
 		System.out.println();
-		System.out.print("Values that are greater than ten: ");
-
-		numbers.forEach(number -> {
-			if (number > 10) {
-				// numBigTen.add(number);
-				System.out.print(number + " ");
-			}
-		});
 	}
 }
